e5d52522a40d35139eca46a6807af6383e2f51bc
mkopsho <michael.kopsho@gmail.com>
2020-10-14 22:27:27 -0400

    Encapsulate initial NPS call in a method and cleanup the image(s) after posting to Twitter

--- main.py
+++ main.py
@@ -3,32 +3,32 @@ import requests
 import random
 import os
 
-# Set API vars
+# Set vars
 twitter_api = TwitterAPI(api_key, api_secret, access_token_key, access_token_secret)
 nps_url = 'https://developer.nps.gov/api/v1/parks?api_key=[YOUR_API_KEY]&limit=500'
-
-# Get random park dict:
-nps_request = requests.get(nps_url)
-park_list = nps_request.json()["data"]
-park_obj = park_list[random.randint(0, len(park_list) - 1)]
-
-# Get `fullName` and NPS website
-full_name = park_obj["fullName"] 
-website = park_obj["url"]
-
-# Get random image from `images` list within park dict:
-image_list = park_obj["images"]
-image = park_obj["images"][random.randint(0, len(image_list) - 1)]["url"]
-
-# Download image
-image_response = requests.get(image)
+park_stats = {}
 
 def download():
+  # Get random park dict:
+  nps_request = requests.get(nps_url)
+  park_list = nps_request.json()["data"]
+  park_obj = park_list[random.randint(0, len(park_list) - 1)]
+
+  # Get `fullName` and NPS website
+  park_stats["full_name"] = park_obj["fullName"] 
+  park_stats["website"] = park_obj["url"]
+
+  # Get random image from `images` list within park dict:
+  image_list = park_obj["images"]
+  image = park_obj["images"][random.randint(0, len(image_list) - 1)]["url"]
+
+  # Download image
+  image_response = requests.get(image)
   if image_response.status_code == 200:
-      with open("IMAGE_PATH", 'wb') as f:
-        f.write(image_response.content)
-        f.close()
-        upload()
+    with open("IMAGE_PATH", 'wb') as f:
+      f.write(image_response.content)
+      f.close()
+      upload()
 
 # Tweet it
 ## Upload image
@@ -47,8 +47,12 @@ def upload():
 def reference(request):
   if request.status_code == 200:
     media_id = request.json()['media_id']
-    request = twitter_api.request('statuses/update', { 'status': full_name + " | " + website, 'media_ids': media_id })
+    request = twitter_api.request('statuses/update', { 'status': park_stats["full_name"] + " | " + park_stats["website"], 'media_ids': media_id })
     print("Success" if request.status_code == 200 else "Failure: " + request.text)
+  # Cleanup
+  if os.path.exists("IMAGE_PATH"):
+    os.remove("IMAGE_PATH")
+  else:
+    print("The file does not exist")
 
 download()
-
